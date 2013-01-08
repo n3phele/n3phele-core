0a3fb65fd72055ba5baa117b2fc1d61cb5c40721
tinhascheibler <tinhascheibler@gmail.com>
2013-01-08 11:07:41 -0200

    Login fixed

--- core-frontend/src/n3phele/service/model/core/Credential.java
+++ core-frontend/src/n3phele/service/model/core/Credential.java
@@ -60,13 +60,13 @@ public class Credential {
 	}
 
 	public Credential decrypt() {
-		String password = Resource.get("password", "");
+		String password = Resource.get("seed", "");
 		return decrypt(this, password);
 	}
 	
 	
 	public static Credential decrypt(Credential credential) {
-		String password = Resource.get("password", "");
+		String password = Resource.get("seed", "");
 		return decrypt(credential, password);
 	}
 
@@ -115,12 +115,12 @@ public class Credential {
 	}
 	
 	public Credential encrypt() {
-		String password = Resource.get("password", "");
+		String password = Resource.get("seed", "");
 		return encrypt(this, password);
 	}
 
 	public static Credential encrypt(Credential credential) {
-		String password = Resource.get("password", "");
+		String password = Resource.get("seed", "");
 		return encrypt(credential, password);
 	}
 
